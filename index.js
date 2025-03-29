cea319606ece917bfef3af12e8b994d477a93317
Rthuro <eh202201050@wmsu.edu.ph>
2025-03-29 14:56:00 +0000

    initial

--- index.js
+++ index.js
@@ -20,10 +20,30 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
 
+app.get("/api/:date?", function (req, res) {
+  let datePass  = req.params.date;
+
+  if (!datePass) {
+    const now = new Date();
+     res.json({ unix: now.getTime(), utc: now.toUTCString() });
+  }
+
+  let pDate = null;
+
+  if(isNaN(datePass)){
+    pDate = new Date(datePass);
+  }else{
+    pDate = new Date(parseInt(datePass))
+  }
+
+  if (pDate.getTime()) {
+    console.log(isNaN(datePass));
+      res.json({ unix: pDate.getTime(), utc: pDate.toUTCString().toString() });
+  }else{  
+    res.json({ error: "Invalid Date" });
+  }
+});
 
 
 // Listen on port set in environment variable or default to 3000
